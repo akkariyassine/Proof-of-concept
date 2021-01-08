90f9a7782c7ff7cad2a3407253afb7048a0ecd08
akkari-yassine <akkari.yassine@cmsservizi.tn>
2021-01-08 15:54:27 +0100

    init api marvel

--- controllers/marvel.controller.js
+++ controllers/marvel.controller.js
@@ -1,23 +1,35 @@
-const api = require("marvel-api");
-const marvel = api.createClient({
-  publicKey: "2688f5e178b1c2781faa3c31bc1fbed9",
-  privateKey: "59298ac0c1fbdb9f31847e189611b6325b3db5d4",
-});
+const publicKey = "2688f5e178b1c2781faa3c31bc1fbed9";
+const privateKey = "59298ac0c1fbdb9f31847e189611b6325b3db5d4";
+const axios = require("axios");
+
+var CryptoJS = require("crypto-js");
 
 var localCache = [];
 exports.getCharacters = (res, req) => {
+  var ts = new Date().getTime();
+  var hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+
+  var url =
+    "http://gateway.marvel.com/v1/public/characters?ts=" +
+    ts +
+    "&apikey=" +
+    publicKey +
+    "&hash=" +
+    hash;
+  console.log(url);
   localCache.map((result) => {
     if (result.page == req.params.nbr) {
       res.status(200).json(result.data);
       return 0;
     }
   });
-  marvel.characters
-    .findAll(req.params.nbr, req.params.index)
-    .then((results) => {
-      localCache.push({ page: req.params.nbr, data: results });
-      res.status(200).json(results);
+
+  return axios
+    .get(url)
+    .then((response) => {
+      res.status(200).json(response.data);
     })
-    .fail(console.error)
-    .done();
+    .catch((err) => {
+      return res.json(err);
+    });
 };
